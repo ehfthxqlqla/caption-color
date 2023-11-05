37e7bf57c48526518e5e4e9e3bf6f1ab7e01af63
ehfthxqlqla <0909aiden@naver.com>
2023-11-05 17:21:45 +0900

    Hello

---
+++ js/index.js
@@ -0,0 +1,35 @@
+const caption = document.querySelector(".caption"),
+ctxMenu =  document.querySelector(".context"),
+colorPickInput = document.querySelector("#colorpick"),
+colorInQuery = new URLSearchParams(window.location.search)
+
+let color = ``
+
+caption.style.color = `#${colorInQuery.get("color")}`
+
+if (!colorInQuery.get("color")) {
+    color = `#ffffff`
+}
+
+window.addEventListener("contextmenu", function(e) {
+    e.preventDefault()
+
+    ctxMenu.style.display = 'block';
+    ctxMenu.style.top = `${e.pageY}px`;
+    ctxMenu.style.left = `${e.pageX}px`;
+})
+
+document.querySelector(".close").addEventListener("click", function(e) {
+    ctxMenu.style.display = 'none';
+    ctxMenu.style.top = null;
+    ctxMenu.style.left = null;
+})
+
+colorPickInput.addEventListener("input", function(e) {
+    color = e.target.value
+    caption.style.color = e.target.value
+})
+
+const share = () => {
+    navigator.clipboard.writeText(`${window.location.href}?color=${color.replace("#", "")}`).then(() => {alert(`성공!`)})
+}
